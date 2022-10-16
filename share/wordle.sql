09b18bf69918ee1391797d57aad95f9ffe9f95ca
JAYRAJARORA <jayraj.arora@gmail.com>
2022-10-16 02:56:44 -0700

    Added games API

--- share/wordle.sql
+++ share/wordle.sql
@@ -1,12 +1,29 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS guesses;
 
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
     username VARCHAR UNIQUE,
-    password VARCHAR
+    password VARCHAR NOT NULL
+);
+-- 0 means game in progress, 1 means game finished
+CREATE TABLE games (
+    game_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    secret_word VARCHAR NOT NULL,
+    game_finished INTEGER DEFAULT 0,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
+CREATE TABLE guesses(
+    guess_id INTEGER PRIMARY KEY,
+    game_id INTEGER NOT NULL,
+    valid_word VARCHAR NOT NULL,
+    guess_number INTEGER NOT NULL,
+    FOREIGN KEY(game_id) REFERENCES games(game_id)
+);
 
 COMMIT;
--- wordle.py
+++ wordle.py
@@ -1,13 +1,15 @@
 # Backend-apps for wordle app
 
 import dataclasses
+import random
 import sqlite3
 import textwrap
+import json
 
 import databases
 import toml
 
-from quart import Quart, g, request, abort, make_response
+from quart import Quart, g, request, abort, make_response, jsonify
 from quart_schema import QuartSchema, RequestSchemaValidationError, validate_request
 
 app = Quart(__name__)
@@ -56,7 +58,7 @@ async def create_user(data):
     try:
         await db.execute(
             """
-                INSERT INTO user(username, password) values (:username, :password)
+                INSERT INTO users(username, password) values (:username, :password)
             """,
             user
         )
@@ -71,20 +73,92 @@ async def create_user(data):
 async def login():
     db = await _get_db()
 
-    auth = request.authorization
+    await check_user(db, request.authorization)
+
+    success_response = {"authenticated": True}
+    return success_response, 200
+
+
+@app.route("/games", methods=["POST"])
+async def create_game():
+    db = await _get_db()
+
+    user_id = await check_user(db, request.authorization)
+
+    # open a file and load json from it
+    answers = load_json_from_file('./share/correct.json')
+    print(len(answers))
+    print(answers)
+    # select a word from list of secret words, word should be different from any word previously assigned to users
+    while True:
+        length = len(answers)
+        chosen_word = answers[random.randint(1, length-1)]
+        print('chosen word : ' + chosen_word)
+        word_exists = await db.fetch_all('SELECT 1 from games where secret_word = :secret_word'
+                                         , values={"secret_word": chosen_word})
+        if not word_exists:
+            break
+
+    game_id = await db.execute("INSERT INTO games(user_id, secret_word) VALUES(:user, :secret_word)"
+                         , values={"user": user_id, "secret_word": chosen_word})
+
+    return {"game_id": game_id, "message": "Game Successfully Created"}, 200
+
+
+@app.route("/games/<int:game_id>/guesses", methods=["POST"])
+async def play_game(game_id):
+    db = await _get_db()
+    data = await request.json
+    if not data:
+        abort(400, "Bad Request: Please enter a word")
+    word = data["word"]
+    if len(word) != 5:
+        abort(400, "Bad Request: Word length should be 5")
+
+    user_id = await check_user(db, request.authorization)
+
+    # open a file and load json from it
+    answers = load_json_from_file('./share/correct.json')
+    valid_words = load_json_from_file('./share/valid.json')
+    total_valid_words = answers + valid_words
+
+    if word not in total_valid_words:
+        abort(400, "Bad Request: Not a valid guess")
+
+    result = await db.fetch_one("SELECT secret_word FROM games WHERE user_id=:user_id AND game_id=:game_id",
+                                values={"game_id": game_id, "user_id": user_id})
+    if not result:
+        abort(400, "No game with this identifier for your username")
+    else:
+        return "Working"
+
+
+async def check_user(db, auth):
     if auth is not None:
         print(auth.type + auth.username + auth.password)
 
     if auth is not None and auth.type == 'basic':
-        user_exists = await db.fetch_one("SELECT 1 from user where username = :username and password =:password",
+        result = await db.fetch_one("SELECT user_id FROM users where username = :username and password =:password",
                                          values={"username": auth.username, "password": auth.password})
-        if user_exists:
-            success_response = {"authenticated": True}
-            return success_response, 200
+        print('inside auth')
+        app.logger.debug(type(result))
+        if result:
+            return result.user_id
         else:
+            print('inside abort')
             abort(401)
     else:
         abort(401)
+
+
+def load_json_from_file(file_name):
+    f = open(file_name)
+    data = json.load(f)
+    values = []
+    # converting unicode to string
+    for item in data:
+        values.append(str(item))
+    return values
 
 
 @app.errorhandler(RequestSchemaValidationError)
@@ -101,3 +175,8 @@ def conflict(e):
 def unauthorized(e):
     print(e)
     return {}, 401, {"WWW-Authenticate": "Basic realm='Wordle Site'"}
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({'message': e.description}), 400
