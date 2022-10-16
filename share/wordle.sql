052001f743ec7ebefa118c8e0481de085ca25e63
JAYRAJARORA <jayraj.arora@gmail.com>
2022-10-16 14:28:19 -0700

    Updated the gameplay of wordle

--- share/wordle.sql
+++ share/wordle.sql
@@ -1,20 +1,21 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS guesses;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY,
     username VARCHAR UNIQUE,
     password VARCHAR NOT NULL
 );
--- 0 means game in progress, 1 means game finished
+-- state - 0 means game in progress, 1 means game finished and won the game, 2 means finished and lost the game
 CREATE TABLE games (
     game_id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     secret_word VARCHAR NOT NULL,
-    game_finished INTEGER DEFAULT 0,
+    state INTEGER DEFAULT 0,
+    guess_remaining INTEGER DEFAULT 6,
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
@@ -25,5 +26,23 @@ CREATE TABLE guesses(
     guess_number INTEGER NOT NULL,
     FOREIGN KEY(game_id) REFERENCES games(game_id)
 );
+
+INSERT INTO users(username, password)
+values
+    ('dummy', 'abc'),
+    ('money', 'abc');
+
+INSERT INTO games(user_id, secret_word, guess_remaining) VALUES
+    (1, 'munch', 5),
+    (1, 'water', 5),
+    (2, 'rebut', 5),
+    (2, 'blush', 4);
+
+INSERT INTO guesses(game_id, valid_word, guess_number) VALUES
+    (1, 'water', 1),
+    (2, 'cater', 1),
+    (3, 'flask', 1),
+    (4, 'anker', 1),
+    (4, 'paste', 2);
 
 COMMIT;
--- wordle.py
+++ wordle.py
@@ -92,7 +92,7 @@ async def create_game():
     # select a word from list of secret words, word should be different from any word previously assigned to users
     while True:
         length = len(answers)
-        chosen_word = answers[random.randint(1, length-1)]
+        chosen_word = answers[random.randint(1, length - 1)]
         print('chosen word : ' + chosen_word)
         word_exists = await db.fetch_all('SELECT 1 from games where secret_word = :secret_word'
                                          , values={"secret_word": chosen_word})
@@ -100,37 +100,122 @@ async def create_game():
             break
 
     game_id = await db.execute("INSERT INTO games(user_id, secret_word) VALUES(:user, :secret_word)"
-                         , values={"user": user_id, "secret_word": chosen_word})
+                               , values={"user": user_id, "secret_word": chosen_word})
 
     return {"game_id": game_id, "message": "Game Successfully Created"}, 200
 
 
-@app.route("/games/<int:game_id>/guesses", methods=["POST"])
+@app.route("/games/<int:game_id>/play", methods=["POST"])
 async def play_game(game_id):
     db = await _get_db()
     data = await request.json
-    if not data:
-        abort(400, "Bad Request: Please enter a word")
-    word = data["word"]
-    if len(word) != 5:
-        abort(400, "Bad Request: Word length should be 5")
 
     user_id = await check_user(db, request.authorization)
+    if data is not None and "word" in data:
+        guess = data["word"]
+        if len(guess) != 5:
+            abort(400, "Bad Request: Word length should be 5")
+        # open a file and load json from it
+        answers = load_json_from_file('./share/correct.json')
+        valid_words = load_json_from_file('./share/valid.json')
+        total_valid_words = answers + valid_words
 
-    # open a file and load json from it
-    answers = load_json_from_file('./share/correct.json')
-    valid_words = load_json_from_file('./share/valid.json')
-    total_valid_words = answers + valid_words
+        if guess not in total_valid_words:
+            abort(400, "Bad Request: Not a valid guess")
 
-    if word not in total_valid_words:
-        abort(400, "Bad Request: Not a valid guess")
+    states = {0: 'In Progress', 1: 'Win', 2: "Loss"}
+    result = await db.fetch_one("SELECT secret_word, guess_remaining, state FROM games WHERE user_id=:user_id "
+                                "AND game_id=:game_id", values={"game_id": game_id, "user_id": user_id})
 
-    result = await db.fetch_one("SELECT secret_word FROM games WHERE user_id=:user_id AND game_id=:game_id",
-                                values={"game_id": game_id, "user_id": user_id})
+    print(result)
     if not result:
         abort(400, "No game with this identifier for your username")
-    else:
-        return "Working"
+    elif result["state"] != 0:
+        return {"number_of_guesses": 6-result["guess_remaining"], "decision": states.get(result["state"])}, 200
+
+    secret_word = result["secret_word"]
+    state = 0
+    guess_remaining = result["guess_remaining"]
+    if data is not None and "word" in data:
+
+        # decrement the guess remaining
+        guess_remaining = guess_remaining - 1
+
+        if guess == secret_word:
+            print("Won the game")
+            state = 1
+
+        # game is over, update the game
+        if guess_remaining == 0 or state:
+            if guess_remaining == 0 and state == 0:
+                state = 2
+            await db.execute("UPDATE games set guess_remaining=:guess_remaining,state=:state  "
+                             "where game_id=:game_id",
+                             values={"guess_remaining": guess_remaining, "game_id": game_id, "state": state})
+            return {"game_id": game_id, "number_of_guesses": guess_remaining, "decision": states[state]}, 200
+        else:
+            await db.execute("UPDATE games set guess_remaining=:guess_remaining where game_id=:game_id",
+                             values={"guess_remaining": guess_remaining, "game_id": game_id})
+
+            guess_number = 6 - guess_remaining
+            await db.execute('INSERT INTO guesses(game_id, valid_word, guess_number) '
+                             'VALUES(:game_id, :valid_word, :guess_number)'
+                             , values={"game_id": game_id, "valid_word": guess, "guess_number": guess_number})
+
+    # prepare the response
+    res = await db.fetch_all('SELECT guess_number, valid_word from guesses '
+                             'where game_id=:game_id order by guess_number',
+                             values={"game_id": game_id})
+    guesses = []
+    for guess_number, valid_word in res:
+        correct_positions, incorrect_positions = compare(secret_word, valid_word)
+        guesses.append(
+            {
+                "guess": valid_word,
+                "guess_number": guess_number,
+                "correct_positions": dict(correct_positions),
+                "incorrect_positions:": dict(incorrect_positions),
+            }
+        )
+
+    return {"guesses": guesses, "guess_remaining": guess_remaining, "game_state": states[state]}, 200
+
+
+def compare(secret_word, guess):
+    secret_word_lst = [i for i in enumerate(secret_word)]
+    guess_list = [i for i in enumerate(guess)]
+    print("Secret word: {}".format(secret_word_lst))
+    print(f"guess: {guess_list}")
+
+    temp_correct_positions = []
+    correct_positions = []
+    incorrect_positions = []
+    for i in range(0, len(secret_word)):
+        if guess_list[i][1] == secret_word_lst[i][1]:
+            temp_correct_positions.append(guess_list[i])
+            correct_positions.append(((guess_list[i][0] + 1), guess_list[i][1]))
+    print('Correct positions: ')
+    print(temp_correct_positions)
+    print(f"Correct index positions are: {correct_positions}")
+    print('List after removing correct ones')
+
+    secret_word_lst = [i for i in secret_word_lst if i not in temp_correct_positions]
+    print(secret_word_lst)
+    guess_list = [i for i in guess_list if i not in temp_correct_positions]
+    print(guess_list)
+
+    for i in range(len(guess_list)):
+        for j in range(len(secret_word_lst)):
+            # found a character which is in a different position
+            if guess_list[i][1] == secret_word_lst[j][1]:
+                incorrect_positions.append((guess_list[i][0] + 1, guess_list[i][1]))
+                secret_word_lst.pop(j)
+                break
+
+    print('Incorrect position')
+    print(incorrect_positions)
+
+    return correct_positions, incorrect_positions
 
 
 async def check_user(db, auth):
@@ -139,7 +224,7 @@ async def check_user(db, auth):
 
     if auth is not None and auth.type == 'basic':
         result = await db.fetch_one("SELECT user_id FROM users where username = :username and password =:password",
-                                         values={"username": auth.username, "password": auth.password})
+                                    values={"username": auth.username, "password": auth.password})
         print('inside auth')
         app.logger.debug(type(result))
         if result:
