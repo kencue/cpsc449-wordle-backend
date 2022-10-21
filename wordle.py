22f42bdad751cfc2a87c88b78e5c7280c07a643c
Brent Pfefferle <brent.pfefferle@gmail.com>
2022-10-21 13:52:52 -0700

    Update wordle.py
    Documentation (comments)

--- wordle.py
+++ wordle.py
@@ -1,29 +1,26 @@
-# Backend-apps for wordle app
-
+# Imports
 import dataclasses
 import random
 import sqlite3
 import textwrap
 import json
-
 import databases
 import toml
-
 from quart import Quart, g, request, abort, jsonify
 from quart_schema import QuartSchema, RequestSchemaValidationError, validate_request
 
+# Initialize the app
 app = Quart(__name__)
 QuartSchema(app)
-
 app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
-
+# Decorator to examine class and find fields
 @dataclasses.dataclass
 class User:
     username: str
     password: str
 
-
+# Establish database connection
 async def _get_db():
     db = getattr(g, "_sqlite_db", None)
     if db is None:
@@ -31,14 +28,14 @@ async def _get_db():
         await db.connect()
     return db
 
-
+# Terminate database connection
 @app.teardown_appcontext
 async def close_connection(exception):
     db = getattr(g, "_sqlite_db", None)
     if db is not None:
         await db.disconnect()
-
-
+       
+# Endpoint for root path, returns HTML.
 @app.route("/", methods=["GET"])
 async def index():
     return textwrap.dedent(
@@ -48,13 +45,13 @@ async def index():
         """
     )
 
-
+# Endpoint for /users, posts user data (username and password)
 @app.route("/users", methods=["POST"])
 @validate_request(User)
 async def create_user(data):
     db = await _get_db()
     user = dataclasses.asdict(data)
-
+    # Insert into database
     try:
         await db.execute(
             """
@@ -62,24 +59,21 @@ async def create_user(data):
             """,
             user
         )
-
+    # Error
     except sqlite3.IntegrityError as e:
         abort(409, e)
-
     return {"Message": "User Successfully Created. Please create a game"}, 201
 
-
+# Endpoint for /login, verifes credentials.
 @app.route("/login", methods=["GET"])
 async def login():
     db = await _get_db()
-
     user_id = await check_user(db, request.authorization)
-
     await db.execute("UPDATE users set is_authenticated = 1 where user_id=:user_id", values={"user_id": user_id})
     success_response = {"authenticated": True}
     return success_response, 200
 
-
+# Endpoint for /games, creates a game for verified user.
 @app.route("/games", methods=["POST"])
 async def create_game():
     db = await _get_db()
@@ -91,9 +85,9 @@ async def create_game():
         abort(401)
     print(res.user_id)
     user_id = res.user_id
-    # open a file and load json from it
+    # Open a file and load json from it
     res = await db.fetch_one("SELECT count(*) count from correct_words")
-    # select a word from list of secret words, word should be different from any word previously assigned to users
+    # Select a word from list of secret words, word should be different from any word previously assigned to users
     length = res.count
     print(length)
     game_id = await db.execute("INSERT INTO games(user_id, secret_word_id) VALUES(:user, :secret_word_id)"
@@ -101,7 +95,7 @@ async def create_game():
 
     return {"game_id": game_id, "message": "Game Successfully Created"}, 200
 
-
+# Endpoint for /stats, data regarding user statistics
 @app.route("/stats",methods=["POST"])
 async def statistics():
     db = await _get_db()
@@ -125,9 +119,9 @@ async def statistics():
             "game_id":game_id
         })
 
-
     return games_stats
 
+# Endpoint for /results, additional statistics.
 @app.route("/results",methods=["POST"])
 async def result():
     db = await _get_db()
@@ -151,11 +145,9 @@ async def result():
             "count":count
         })
 
-
     return games_stats
 
-
-
+# Endpoint /games/<int:game_id>/play, Core game logic.
 @app.route("/games/<int:game_id>/play", methods=["POST"])
 async def play_game(game_id):
     db = await _get_db()
@@ -174,14 +166,12 @@ async def play_game(game_id):
         guess = data["word"]
         if len(guess) != 5:
             abort(400, "Bad Request: Word length should be 5")
-        # open a file and load json from it
+        # Open a file and load json from it
         res0 = await db.fetch_one("SELECT correct_word_id from correct_words WHERE correct_word =:word", values={"word":guess})
         res = await db.fetch_one("SELECT valid_word_id from valid_words WHERE valid_word =:word", values={"word":guess})
         if not res:
             if not res0:
                 abort(400, "Bad Request: Not a valid guess")
-
-
 
     states = {0: 'In Progress', 1: 'Win', 2: "Loss"}
     result = await db.fetch_one("SELECT correct_words.correct_word secret_word, guess_remaining, state FROM games "
@@ -200,14 +190,14 @@ async def play_game(game_id):
     guess_remaining = result["guess_remaining"]
     if data is not None and "word" in data:
 
-        # decrement the guess remaining
+        # Decrement the guess remaining
         guess_remaining = guess_remaining - 1
 
         if guess == secret_word:
             print("Won the game")
             state = 1
 
-        # game is over, update the game
+        # Game is over, update the game
         if guess_remaining == 0 or state:
             if guess_remaining == 0 and state == 0:
                 state = 2
@@ -224,7 +214,7 @@ async def play_game(game_id):
             await db.execute('INSERT INTO guesses(game_id, valid_word_id, guess_number) '
                              'VALUES(:game_id, :valid_word_id, :guess_number)'
                              , values={"game_id": game_id, "valid_word_id": valid_word_id, "guess_number": guess_number})
-    # prepare the response
+    # Prepare the response
     res = await db.fetch_all("SELECT guess_number, valid_words.valid_word from guesses join "
                              "valid_words where game_id=:game_id "
                              "and valid_words.valid_word_id=guesses.valid_word_id "
@@ -245,7 +235,7 @@ async def play_game(game_id):
 
     return {"guesses": guesses, "guess_remaining": guess_remaining, "game_state": states[state]}, 200
 
-
+# Function to compare the guess to answer.
 def compare(secret_word, guess):
     secret_word_lst = [i for i in enumerate(secret_word)]
     guess_list = [i for i in enumerate(guess)]
@@ -282,7 +272,7 @@ def compare(secret_word, guess):
 
     return correct_positions, incorrect_positions
 
-
+# User authentication.
 async def check_user(db, auth):
     if auth is not None:
         print(auth.type + auth.username + auth.password)
@@ -300,33 +290,32 @@ async def check_user(db, auth):
     else:
         abort(401)
 
-
+# Load file and convert unicode to string.
 def load_json_from_file(file_name):
     f = open(file_name)
     data = json.load(f)
     values = []
-    # converting unicode to string
     for item in data:
         values.append(str(item))
     return values
 
-
+# Error status: Client error.
 @app.errorhandler(RequestSchemaValidationError)
 def bad_request(e):
     return {"error": str(e.validation_error)}, 400
 
-
+# Error status: Cannot process request.
 @app.errorhandler(409)
 def conflict(e):
     return {"error": str(e)}, 409
 
-
+# Error status: Unauthorized client.
 @app.errorhandler(401)
 def unauthorized(e):
     print(e)
     return {}, 401, {"WWW-Authenticate": "Basic realm='Wordle Site'"}
 
-
+# Error status: Cannot or will not process the request.
 @app.errorhandler(400)
 def bad_request(e):
     return jsonify({'message': e.description}), 400
