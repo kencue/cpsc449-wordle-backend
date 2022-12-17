e6f218593d08bb061b842bc6186ce61212b53535
Abhishek Shinde <ashinde813@gmail.com>
2022-12-16 21:53:38 -0800

    Client URL

--- leaderboard.py
+++ leaderboard.py
@@ -1,6 +1,9 @@
 import dataclasses
 import toml
 import redis
+import os
+import socket
+import httpx
 from quart import Quart, g, request, abort, jsonify
 from quart_schema import QuartSchema, RequestSchemaValidationError, validate_request, tag
 
@@ -90,3 +93,11 @@ def unauthorized(e):
 @app.errorhandler(400)
 def bad_request(e):
     return jsonify({'message': e.description}), 400
+
+def url_registeration():
+    url = "http://" + socket.gethostbyname(socket.getfqdn(""))+":"+ os.environ['PORT'] + "/leaderboard/add"
+    print(url)
+    data = {"url" :url }
+    response = httpx.post("http://tuffix-vm/games/urlregisteration",json = data)
+
+url_registeration()
